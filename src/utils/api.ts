80366bc34ef9241588f5384c238528607842c1c5
rayhan_abdullha <captainrayhan215@gmail.com>
2026-05-25 12:20:45 +0600

    fix: remove localhost and deployed api link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api/v1",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "https://mpms-ucmy.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
